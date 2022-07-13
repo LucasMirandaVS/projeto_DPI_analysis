2c8803a6b9c297fd07af8139149b59aae2693f37
Lucas Miranda <77032413+LucasMirandaVS@users.noreply.github.com>
2022-07-13 19:29:40 -0300

    Update Limpeza dos dados.R

--- Limpeza dos dados.R
+++ Limpeza dos dados.R
@@ -4,13 +4,13 @@ library(readr)
 # Importando os dados
 data <- read_csv("C:/Users/Lucas/Desktop/projeto/DPI2020.csv")
 
-# Definido o escopo da análise: filtrando os países e as variáveis
-df <- data %>%
+# Definido o escopo da análise: filtrando os países e o período
+df1 <- data %>%
   arrange(countryname)
   
-df <- df[, c('countryname','execnat', 'system', 'yrsoffc',
+df1 <- df1[, c('countryname','execnat', 'system', 'yrsoffc',
              'finittrm','execrlc','execrel', 'reelect', 
-             'prtyin', 'year', 'termlimit', 'herfgov', 'herfopp')] %>%
+             'year', 'termlimit')] %>%
   rename('Anos de governo' = 'yrsoffc',
          'Religião' = 'execrel',
          'Ideologia' = 'execrlc',
@@ -19,17 +19,16 @@ df <- df[, c('countryname','execnat', 'system', 'yrsoffc',
          'Ano' = 'year',
          'Mandato finito' = 'finittrm',
          'Regime' = 'system',
-         'Anos de Mandato' = 'prtyin',
          'Nacionalista' = 'execnat',
-         'Restrições à reeleição' = 'termlimit',
-         'Herfindal(Governo)' = 'herfgov',
-         'Herfindal(Oposição)' = 'herfopp') %>%
+         'Restrições à reeleição' = 'termlimit') %>%
   mutate(País = recode(País, "FRG/Germany" = "Germany")) %>%
   dplyr :: na_if(-999) %>%
   dplyr :: na_if('')
 
+summary(df1)
+
 # Definindo as variáveis categóricas  e atribuindo nomes às categorias
-df$Regime <- factor(df$Regime,
+df1$Regime <- factor(df1$Regime,
                     levels = c('Presidential',
                                'Assembly-Elected President',
                                'Parliamentary'),
@@ -37,15 +36,15 @@ df$Regime <- factor(df$Regime,
                                'Presidente eleito pelo parlamento',
                                'Parlamentarismo'))
 
-df$`Mandato finito` <- factor(df$`Mandato finito`,
+df1$`Mandato finito` <- factor(df1$`Mandato finito`,
                               levels = c(0, 1),
                               labels = c('Não', 'Sim'))
 
-df$Reeleição <- factor(df$Reeleição,
+df1$Reeleição <- factor(df1$Reeleição,
                        levels = c(0,1),
                        labels = c('Não', 'Sim'))
 
-df$Ideologia <- factor(df$Ideologia,
+df1$Ideologia <- factor(df1$Ideologia,
                        levels = c(0,
                                   'Right',
                                   'Center',
@@ -54,7 +53,7 @@ df$Ideologia <- factor(df$Ideologia,
                                   'Direita',
                                   'Centro',
                                   'Esquerda'))
-df$Religião <- factor(df$Religião,
+df1$Religião <- factor(df1$Religião,
                       levels = c(0,
                                  'Christian',
                                  'Catholic',
@@ -65,11 +64,11 @@ df$Religião <- factor(df$Religião,
                                  'Católico',
                                  'Islã',
                                  'Hindu'))
-df$Nacionalista <- factor(df$Nacionalista,
+df1$Nacionalista <- factor(df1$Nacionalista,
                           levels = c(0,1),
                           labels = c('Não', 'Sim'))
 
-df$`Restrições à reeleição` <- factor(df$`Restrições à reeleição`,
+df1$`Restrições à reeleição` <- factor(df1$`Restrições à reeleição`,
                                       levels = c(1,2,3,4,5,6,7,8),
                                       labels = c('Sem limitações',
                                                  'limitação a mandatos consecutivos',
@@ -79,3 +78,6 @@ df$`Restrições à reeleição` <- factor(df$`Restrições à reeleição`,
                                                  'Até 3 mandatos consecutivos',
                                                  'Reeleição possível se ficar 2 mandatos fora',
                                                  'Até dois mandatos'))
+
+# Salvando os dados
+save(df1, file = 'saved.dt1.RData')
